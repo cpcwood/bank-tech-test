a4ab10adbc88563a7a9950242e148f5deca1f3e1
cpcwood <cpcwood@outlook.com>
2020-02-24 17:07:11 +0000

    Withdraw method added which allow money to be withrawn from account, one parameter (amount_being_withdrawn)

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,4 +1,6 @@
 # Bank account class allow user to deposit and withdraw money, and show statment
 class BankAccount
   def deposit(amount_being_deposited); end
+
+  def withdraw(amount_being_withdrawn); end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -2,8 +2,14 @@ require 'bank_account'
 
 describe BankAccount do
   describe '#deposit' do
-    it 'money can be deposited' do
+    it 'allows amount of money can be deposited' do
       expect(subject).to respond_to(:deposit).with(1).argument
+    end
+  end
+
+  describe '#withdraw' do
+    it 'allows amount of money to be withdrawn' do
+      expect(subject).to respond_to(:withdraw).with(1).argument
     end
   end
 end
