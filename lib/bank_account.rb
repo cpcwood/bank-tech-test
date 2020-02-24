1528c8ec0a78ec763d23e64f2264265a4ea4aff2
cpcwood <cpcwood@outlook.com>
2020-02-24 21:18:05 +0000

    #statement now returns multiple transaction histories

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,21 +2,31 @@
 class BankAccount
   def initialize
     @balance = 0
+    @transaction_history = []
   end
 
   def deposit(amount_being_deposited)
     @balance += amount_being_deposited
+    @transaction_history.push({ credit: amount_being_deposited, balance: @balance })
   end
 
   def withdraw(amount_being_withdrawn)
     @balance -= amount_being_withdrawn
-    @transaction_history = { debit: amount_being_withdrawn }
+    @transaction_history.push({ debit: amount_being_withdrawn, balance: @balance })
     amount_being_withdrawn
   end
 
   def statement
-    top_line = "debit || balance\n"
-    statement_line = "#{@transaction_history[:debit] if @transaction_history} || #{@balance}"
-    top_line + statement_line
+    statement = "debit || balance\n"
+    @transaction_history.each do |record|
+      statement += generate_statement_line(record)
+    end
+    statement
+  end
+
+  private
+
+  def generate_statement_line(record)
+    "#{record[:debit] || record[:credit]} || #{record[:balance]}\n"
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -29,9 +29,6 @@ describe BankAccount do
     it 'has method to return statement' do
       expect(subject).to respond_to(:statement)
     end
-    it 'returns the current balance of the account' do
-      expect(subject.statement).to include('0')
-    end
     it 'displays header of balance on top line' do
       top_line = subject.statement.split("\n")[0]
       expect(top_line).to include('balance')
@@ -43,6 +40,12 @@ describe BankAccount do
     it 'returns history of debit with associated balance' do
       subject.withdraw(10)
       expect(subject.statement).to include('10 || -10')
+    end
+    it 'returns history of multiple debits each on a newline' do
+      subject.withdraw(10)
+      subject.withdraw(2)
+      expect(subject.statement).to include("10 || -10\n")
+      expect(subject.statement).to include("2 || -12\n")
     end
   end
 end
