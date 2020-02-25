000feed9f735a05fe877c2d3e8ea175416ae2a1b
cpcwood <cpcwood@outlook.com>
2020-02-25 10:33:06 +0000

    #start_banking refactored to create new method #print_output which prints outputs to the console

--- lib/bank_ui.rb
+++ lib/bank_ui.rb
@@ -1,6 +1,10 @@
 # User interface for CLI Bank, accepts Bank class in initalization as backend logic
 class BankUi
   def start_banking
-    STDOUT.puts('Welcome to CLI Bank')
+    print_output('Welcome to CLI Bank')
+  end
+  
+  def print_output(output)
+    STDOUT.puts(output)
   end
 end
--- spec/bank_ui_spec.rb
+++ spec/bank_ui_spec.rb
@@ -3,8 +3,15 @@ require 'bank_ui'
 describe BankUi do
   describe '#start_banking' do
     it 'Welcomes user to banking' do
-      expect(STDOUT).to receive(:puts).with('Welcome to CLI Bank')
+      expect(subject).to receive(:print_output).with('Welcome to CLI Bank')
       subject.start_banking
+    end
+  end
+
+  describe '#print_output' do
+    it 'Prints argument to the console' do
+      expect(STDOUT).to receive(:puts).with('test')
+      subject.print_output('test')
     end
   end
 end
