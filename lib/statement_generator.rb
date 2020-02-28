736052b3b80250c4993fc57a8685f4e810d24e88
cpcwood <cpcwood@outlook.com>
2020-02-28 20:01:06 +0000

    StatementGenerator#print_statement now returns credit history

--- lib/statement_generator.rb
+++ lib/statement_generator.rb
@@ -1,5 +1,20 @@
 class StatementGenerator
   def create_statement(transaction_history)
-    "date || credit || debit || balance\n"
+    statement = "date || credit || debit || balance\n"
+    transaction_history.each do |record|
+      statement += generate_statement_line(record)
+    end
+    statement
   end
+
+  private
+
+  def generate_statement_line(record)
+    "#{record[:date].strftime('%d/%m/%Y')} ||#{str_value(record[:credit])}||#{str_value(record[:debit])}||#{str_value(record[:balance])}\n"
+  end
+
+  def str_value(value)
+    value ? " #{format('%<value>.2f', value: value)} " : ' '
+  end
+
 end
--- spec/statement_generator_spec.rb
+++ spec/statement_generator_spec.rb
@@ -1,8 +1,8 @@
 require 'statement_generator'
 
 describe StatementGenerator do
-  transaction_history = [{ date: Time.new(2012, 1, 10), credit: 92.12, balance: @balance },
-    { date: Time.new(2012, 1, 14), credit: 120, balance: @balance }]
+  transaction_history = [{ date: Time.new(2012, 1, 10), credit: 92.12, balance:  92.12},
+    { date: Time.new(2012, 1, 14), debit: 120, balance: -27.79 }]
 
   describe '#create_statement' do
     it 'has method to return statement from a transaction history' do
@@ -11,6 +11,10 @@ describe StatementGenerator do
     it 'displays header of (date, credit, debit, balance) on top line' do
       top_line = subject.create_statement(transaction_history).split("\n")[0]
       expect(top_line).to eq("date || credit || debit || balance")
+    end
+    it 'prints credit statement' do
+      result = subject.create_statement(transaction_history).split("\n")
+      expect(result[1]).to include('10/01/2012 || 92.12 || || 92.12')
     end
   end
 end
